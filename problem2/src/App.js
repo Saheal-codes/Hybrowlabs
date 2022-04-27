a023527080e9c9e64dc53051beb99a11449ecc13
Sahil <mydocs3000@gmail.com>
2022-04-27 22:49:59 +0530

    init commit 2

---
+++ problem2/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* const randomNumber = Math.floor(Math.random() * 10);
+const url = `https://swapi.dev/api/people/${randomNumber}`;
+const response = await axios.get(url);
+res.send(response.data); */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
