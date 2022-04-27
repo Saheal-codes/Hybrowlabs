e11bab0c4ea4d996f0636fad360484b2c3244306
Sahil <mydocs3000@gmail.com>
2022-04-27 21:36:04 +0530

    Problem 1 done

--- problem1/src/App.js
+++ problem1/src/App.js
@@ -1,9 +1,45 @@
+import { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [string, setstring] = useState("");
+  const [output, setoutput] = useState("");
+
+  const submithandler = useCallback(
+    (event) => {
+      event.preventDefault();
+      if (string.length === 0) {
+        return setoutput("Please enter a string");
+      }
+      let stringcopy = string
+        .split("")
+        .filter((x) => x !== " ")
+        .map((x) => x.toUpperCase());
+      let map = {};
+      for (let i = 0; i < stringcopy.length; i++) {
+        if (map[stringcopy[i]]) {
+          map[stringcopy[i]] += 1;
+        } else {
+          map[stringcopy[i]] = 1;
+        }
+      }
+      let result = Object.keys(map).map((x) => `${x} - ${map[x]}`);
+      setoutput(result.join("\n"));
+    },
+    [string]
+  );
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <input
+          type="text"
+          value={string}
+          onChange={(e) => setstring(e.target.value)}
+          placeholder="Enter any string"
+        />
+        <button onClick={submithandler}>Submit</button>
+        <pre>{output}</pre>
+      </header>
     </div>
   );
 }
